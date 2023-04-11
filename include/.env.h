dff55689a4f89f980ac3fe43c5245a54d975801e
Kevin Kharkov <kkharkov@gwu.edu>
2023-04-11 00:29:56 -0400

    config additions for testing

--- include/.env.h
+++ include/.env.h
@@ -10,11 +10,13 @@
 // #define PASS "Room936BungusChutts"
 
 // #define URL "172.20.10.6"
-#define URL "192.168.12.239"
+// #define URL "192.168.12.239"
 // #define URL "172.31.34.178"
-// #define URL "192.168.12.75"
+#define URL "192.168.12.75"
 
-#define LPORT 5173
+// #define LPORT 5173
+#define LPORT 5174
+
 // #define LPORT 4173
 
 #endif
